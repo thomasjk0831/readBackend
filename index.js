6df08891dbddd5692a8971f0e24e6c667ad22711
Thomas Kim <Thomasjk0831@gmail.com>
2020-12-08 11:15:19 -0500

    fd

--- index.js
+++ index.js
@@ -1,4 +1,9 @@
 const server = require("./api/server")
+// const wakeUpDyno = require('./wokeDyno.js')
+
 
 const port = process.env.PORT || 5000
-server.listen(port, () => { console.log(`--server running on port ${port}`) })
+server.listen(port, () => {
+    // wakeUpDyno(DYNO_URL)
+    console.log(`--server running on port ${port}`)
+})
