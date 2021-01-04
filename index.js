b0be64f5591ab7da3ccd4619b53a55bdd3f395f1
Thomas Kim <Thomasjk0831@gmail.com>
2021-01-04 11:32:56 -0500

    added heroku workaround

--- index.js
+++ index.js
@@ -1,9 +1,12 @@
 const server = require("./api/server")
-// const wakeUpDyno = require('./wokeDyno.js')
+const wakeUpDyno = require('./wokeDyno.js')
 
 
 const port = process.env.PORT || 5000
+
+const DYNO_URL = "https://readit-backend.herokuapp.com/"
+
 server.listen(port, () => {
-    // wakeUpDyno(DYNO_URL)
+    wakeUpDyno(DYNO_URL)
     console.log(`--server running on port ${port}`)
 })
