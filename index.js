84b34e991a767b78b109493f4b84901153767701
Thomas Kim <Thomasjk0831@gmail.com>
2021-01-24 10:22:42 -0500

    disabled dyno hack for heroku

--- index.js
+++ index.js
@@ -7,6 +7,6 @@ const port = process.env.PORT || 5000
 const DYNO_URL = "https://readit-backend.herokuapp.com/"
 
 server.listen(port, () => {
-    wakeUpDyno(DYNO_URL)
+    // wakeUpDyno(DYNO_URL)
     console.log(`--server running on port ${port}`)
 })
