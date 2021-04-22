aaa69ac4cd4063a67dd034bc7163c894487c337b
thomasjk0831 <thomaskim08310@gmail.com>
2021-04-22 15:36:50 -0400

    dyno

--- api/server.js
+++ api/server.js
@@ -1,25 +1,24 @@
-const express = require('express')
-const server = express()
-const cors = require('cors')
+const express = require("express");
+const server = express();
+const cors = require("cors");
 
-const authRouter = require('../auth/auth-router')
-const userRouter = require('../users/users-router')
-const postRouter = require('../posts/posts-router')
-const commentRouter = require('../comments/comments-router')
-const subRouter = require("../subreadits/subreadits-router")
+const authRouter = require("../auth/auth-router");
+const userRouter = require("../users/users-router");
+const postRouter = require("../posts/posts-router");
+const commentRouter = require("../comments/comments-router");
+const subRouter = require("../subreadits/subreadits-router");
 
-server.use(express.json())
-server.use(cors())
+server.use(express.json());
+server.use(cors());
 
-server.get('/', (req, res) => {
-    res.json({ msg: "api is up" })
-})
+server.get("/", (req, res) => {
+  res.json({ msg: "readit api is up" });
+});
 
-server.use('/api/auth', authRouter)
-server.use('/api/user', userRouter)
-server.use('/api/post', postRouter)
-server.use('/api/comment', commentRouter)
-server.use("/r", subRouter)
+server.use("/api/auth", authRouter);
+server.use("/api/user", userRouter);
+server.use("/api/post", postRouter);
+server.use("/api/comment", commentRouter);
+server.use("/r", subRouter);
 
-module.exports = server
-
+module.exports = server;
